60ad8c8ef2ce357c7b2e4f95acdd510350aaec98
wlgml9078 <wlgml9078@nate.com>
2015-11-17 18:28:03 +0900

    프로젝트 시작!

---
+++ Scheduling-Jh/main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Scheduling_Jh
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("즐","안녕안녕");
+        }
+
+        private void btn1_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("바이바이");
+        }
+    }
+}
